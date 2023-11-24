9ea802acedc8a6bd1093c8b4403dd56e71f38c20
Hung-En Liao <Hung-En Liao>
2023-11-24 10:13:13 -0400

    init commit

---
+++ backend/models/userModel.js
@@ -0,0 +1,46 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const validator = require("validator");
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+// static signup method
+userSchema.statics.signup = async function (email, password) {
+  //validation
+  if (!email || !password) {
+    throw Error("All fields must be filled");
+  }
+  if (!validator.isEmail(email)) {
+    throw Error("Email is not valid");
+  }
+  if (!validator.isStrongPassword(password)) {
+    throw Error(
+      "The password must be at least eight characters long and contain an uppercase letter, a lowercase letter, a number, and a special symbol."
+    );
+  }
+  const exist = await this.findOne({ email });
+
+  if (exist) {
+    throw Error("email already in use");
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  const hash = await bcrypt.hash(password, salt);
+
+  const user = await this.create({ email, password: hash });
+
+  return user;
+};
+module.exports = mongoose.model("User", userSchema);
