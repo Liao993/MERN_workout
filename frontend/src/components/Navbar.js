8537f2bf6b718fbfa745fd1420bad9e1b7a3106e
Hung-En Liao <Hung-En Liao>
2023-11-25 13:43:15 -0400

    nav bar

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,16 +1,20 @@
-import { Link } from 'react-router-dom'
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
-
   return (
     <header>
       <div className="container">
         <Link to="/">
           <h1>Workout Buddy</h1>
         </Link>
+        <nav>
+          <Link to="/">Home</Link>
+          <Link to="/signup">Sign Up</Link>
+          <Link to="/login">Log In</Link>
+        </nav>
       </div>
     </header>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
